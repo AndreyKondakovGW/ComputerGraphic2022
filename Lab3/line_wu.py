1325c98fe7ade38455e549a8471f66eaefe05c71
AndreyKondakovGW <andreykondakov2302@gmail.com>
2022-09-30 16:04:46 +0300

    merge lab3-2 branch

--- Lab3/line_wu.py
+++ Lab3/line_wu.py
@@ -13,7 +13,7 @@ def draw_pix(img, point, alpha, bg, color=(255, 0, 0)):
     img.put(rgb2hex(color), point)
 
 
-def line(img, x1, y1, x2, y2, color, bg):
+def line(img, x1, y1, x2, y2,bg,  color=(255, 0, 0)):
     if x2 < x1:
         x2, x1 = x1, x2
         y2, y1 = y1, y2
@@ -25,8 +25,8 @@ def line(img, x1, y1, x2, y2, color, bg):
     x_gap = 1 - (x1 + 0.5) % 1
     x_pxl1 = x_end
     y_pxl1 = int(y_end // 1)
-    draw_pix(img, (x_pxl1, y_pxl1), (1 - y_end % 1) * x_gap, bg, color=(255, 0, 0))
-    draw_pix(img, (x_pxl1, y_pxl1 + 1), y_end % 1 * x_gap, bg, color=(255, 0, 0))
+    draw_pix(img, (x_pxl1, y_pxl1), (1 - y_end % 1) * x_gap, bg, color= color)
+    draw_pix(img, (x_pxl1, y_pxl1 + 1), y_end % 1 * x_gap, bg, color= color)
     inter_y = y_end + gradient
 
     x_end = round(x2)
@@ -34,31 +34,32 @@ def line(img, x1, y1, x2, y2, color, bg):
     x_gap = (x2 + 0.5) % 1
     x_pxl2 = x_end
     y_pxl2 = y_end // 1
-    draw_pix(img, (x_pxl2, int(y_pxl2)), (1 - y_end % 1) * x_gap, bg, color=(255, 0, 0))
-    draw_pix(img, (x_pxl2, int(y_pxl2 + 1)), y_end % 1 * x_gap, bg, color=(255, 0, 0))
+    draw_pix(img, (x_pxl2, int(y_pxl2)), (1 - y_end % 1) * x_gap, bg, color= color)
+    draw_pix(img, (x_pxl2, int(y_pxl2 + 1)), y_end % 1 * x_gap, bg, color= color)
 
     for x in range(x_pxl1 + 1, x_pxl2 - 1):
-        draw_pix(img, (x, int(inter_y)), 1 - inter_y % 1, bg, color=(255, 0, 0))
-        draw_pix(img, (x, int(inter_y) + 1), inter_y % 1, bg, color=(255, 0, 0))
+        draw_pix(img, (x, int(inter_y)), 1 - inter_y % 1, bg, color= color)
+        draw_pix(img, (x, int(inter_y) + 1), inter_y % 1, bg, color= color)
         inter_y = inter_y + gradient
 
 
-root_main = Tk()
-root_main.title("Line Wu")
+if __name__ == "__main__":
+    root_main = Tk()
+    root_main.title("Line Wu")
 
-win_width = root_main.winfo_screenwidth() // 2
-win_height = root_main.winfo_screenheight() // 2
-bg = (255, 255, 255)
+    win_width = root_main.winfo_screenwidth() // 2
+    win_height = root_main.winfo_screenheight() // 2
+    bg = (255, 255, 255)
 
-canv = Canvas(root_main, width=win_width, height=win_height, bg="white")
-img = PhotoImage(width=win_width, height=win_height)
-canv.create_image((win_width / 2, win_height / 2), image=img, state="normal")
+    canv = Canvas(root_main, width=win_width, height=win_height, bg="white")
+    img = PhotoImage(width=win_width, height=win_height)
+    canv.create_image((win_width / 2, win_height / 2), image=img, state="normal")
 
-c1 = (255, 0, 0)
-x1 = 300
-x2 = 100
-y1 = 100
-y2 = 400
-line(img, x1, y1, x2, y2, c1, bg)
-canv.pack()
-root_main.mainloop()
+    c1 = (0, 0, 255)
+    x1 = 300
+    x2 = 100
+    y1 = 100
+    y2 = 400
+    line(img, x1, y1, x2, y2,bg, c1)
+    canv.pack()
+    root_main.mainloop()
