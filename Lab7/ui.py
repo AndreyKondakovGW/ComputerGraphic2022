5253129f4481ae3311658c458d24a78857fdbab6
caisilus <serangor@gmail.com>
2022-11-08 20:51:04 +0300

    Fix lab 7 after refactor

---
+++ Lab7/ui.py
@@ -0,0 +1,15 @@
+from Lab6.ui3D import UI3D
+from Lab7.obj_parser import ObjParser
+
+class UI7(UI3D):
+    def __init__(self):
+        self.loader = ObjParser()
+        super().__init__()
+
+    def add_leftmost_buttons(self):
+        super().add_leftmost_buttons()
+        self.add_button("Save", lambda: self.loader.save_figure("Lab7/models/saved.obj", self.scene.storage))
+
+    def init_sidemenu_instruments(self):
+        super().init_sidemenu_instruments()
+        self.side_menu_controller.add_instrument("Load Figure", lambda: self.loader.open_file())
